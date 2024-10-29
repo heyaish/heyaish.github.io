792e34213c4696ff71760db6655aa32d8c9b4d68
heyaish <aishwarya.social@gmail.com>
2024-10-29 02:32:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("header.html")
+      .then(response => response.text())
+      .then(data => document.getElementById("my-header").innerHTML = data);
+
+});
